739f007e6c89a97e63d15d2e8507c53830e96b82
imarquina <imarquina@gmail.com>
2017-05-11 18:51:03 +0200

    Initial commit

--- js.src/im_app.constantes.js
+++ js.src/im_app.constantes.js
@@ -1,15 +1,16 @@
 // JavaScript Document
-App.Constantes={
-	si:1,
-	no:0,
-	cadenaVacia:'',
-	origenInicio:'inicio',
-	origenGaleria:'galeria',
-	origenNews:'news',
-	appFlickr:'f',
-	appPinterest:'p',
-	moneda:' €',
-	copia:'Precio copia :',
-	dimesion:'Dim.: ',
-	pagDetalle:'detail.html'
+App.Constantes = {
+    si: 1,
+    no: 0,
+    cadenaVacia: '',
+    origenInicio: 'inicio',
+    origenGaleria: 'galeria',
+    origenNews: 'news',
+    appFlickr: 'f',
+    appPinterest: 'p',
+    app500px: '5',
+    moneda: ' €',
+    copia: 'Precio copia :',
+    dimesion: 'Dim.: ',
+    pagDetalle: 'detail.html'
 }
